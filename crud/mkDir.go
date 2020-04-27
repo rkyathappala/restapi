8603c048523b56492251ae0cf7f0ae6de28c5dfb
Rahul Kyathappala <rkyathappala@gmail.com>
2020-04-27 01:14:15 -0400

    Started directory maker

---
+++ crud/mkDir.go
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////////////////////////////
+// Rahul Kyathappala
+// 29 March 2020
+//
+// Make directory routine for CRUD file managment REST API
+///////////////////////////////////////////////////////////////////////////////
+
+package crud
+/*
+import (
+  "fmt"
+  //"path"
+  //"strings"
+  "strconv"
+  "net/http"
+  "encoding/json"
+
+  "github.com/gorilla/mux"
+  "restapi/element"
+)
+*/import "net/http"
+// MkDir makes a directory
+func MkDir(w http.ResponseWriter, r *http.Request) {
+
+}
